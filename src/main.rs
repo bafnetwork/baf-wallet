3a5151fdfc0d4fcfa4ae18d14cd0125989334bfd
Sebastien La Duca <sladuca777@gmail.com>
2020-12-30 20:04:10 -0500

    hash/verify passwords, store stuff in rocksdb

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
+use argonautica::{Hasher, Verifier};
+use http::StatusCode;
 use hyper::server::Server;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response};
 use rocksdb::{Options, DB};
-use secrecy::SecretVec;
-use serde::{Serialize, Deserialize};
+use secrecy::{Secret, SecretVec};
+use serde::{Deserialize, Serialize};
 use serde_json::value::Value;
+use std::error::Error;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::runtime::Runtime;
 use tokio::sync::oneshot;
-use http::StatusCode;
-
+use uuid::Uuid;
 /// RocksDB stores only 2 mappings:
 /// Email -> UserAccount
 /// NearAccountID -> PrivKey
@@ -20,44 +22,160 @@ struct KeyPair {
     pub_key: Vec<u8>,
 }
 
-struct UserAccount {
-    id: u64,
-    email: String,
+#[derive(Serialize, Deserialize)]
+struct Web2AuthRecord<'a> {
+    password_hash: &'a str,
+    id: u128,
+}
+
+struct Account<'a> {
+    email: &'a str,
     near_account_id: Option<String>,
-    hashed_password: Vec<u8>,
+}
+
+#[derive(Deserialize)]
+struct SignupArgs<'a> {
+    email: &'a str,
+    password: &'a str,
+}
+
+#[derive(Deserialize)]
+struct LoginArgs<'a> {
+    email: &'a str,
+    password: &'a str,
+}
+
+fn get_jwt_secret<'a>() -> Secret<&'a [u8]> {
+    match std::env::var("JWT_SECRET") {
+        Ok(secret) => Secret::new(secret.as_bytes()),
+        Err(_) => panic!("JWT_SECRET environment variable not set!"),
+    }
+}
+
+fn get_hasher_secret<'a>() -> Secret<&'a [u8]> {
+    match std::env::var("HASHER_SECRET") {
+        Ok(secret) => Secret::new(secret.as_bytes()),
+        Err(_) => panic!("HASHER_SECRET environment variable not set!"),
+    }
+}
+
+fn bad_request(e: Option<Box<dyn Error>>) -> Response<Body> {
+    if let Some(e) = e {
+        eprintln!("{}", e);
+    }
+    let mut res = Response::new(Body::from("Bad Request"));
+    *res.status_mut() = StatusCode::BAD_REQUEST;
+    return res;
+}
+
+fn internal_server_error(e: Option<Box<dyn Error>>) -> Response<Body> {
+    if let Some(e) = e {
+        eprintln!("{}", e);
+    }
+    let mut res = Response::new(Body::from("Internal Server Error"));
+    *res.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+    return res;
 }
 
 /// Handler for all incoming RPC's. matches on the RPC's method and route and executes the
 /// corresponding RPC
-async fn handler(req: Request<Body>, _db: Arc<DB>) -> Result<Response<Body>, hyper::Error> {
+async fn handler<'a>(
+    req: Request<Body>,
+    db: Arc<DB>,
+    hasher_secret: Secret<&'a [u8]>,
+    jwt_secret: Secret<&'a [u8]>,
+) -> Result<Response<Body>, hyper::Error> {
     match (req.method(), req.uri().path()) {
         (&Method::POST, "/rpc") => {
             let body = hyper::body::to_bytes(req.into_body()).await?;
             let rpc: JsonRPC = match serde_json::from_slice(body.as_ref()) {
                 Ok(json_rpc) => json_rpc,
                 Err(e) => {
-                    eprintln!("{}", e);
-                    let mut res = Response::new(Body::from("Bad Request"));
-                    *res.status_mut() = StatusCode::BAD_REQUEST;
-                    return Ok(res);
+                    return Ok(bad_request(Some(e.into())));
                 }
             };
 
-            println!(
-                "method: {:#?}, id: {:#?}",
-                rpc.method, rpc.id
-            );
+            println!("method: {:#?}, id: {:#?}", rpc.method, rpc.id);
 
-            match rpc.params {
-                Some(_params) => {
-                    unimplemented!()
-                }
+            match *&rpc.method {
+                Some("createBafAccount") => {}
+                Some("createNearAccount") => {}
+                Some("signTx") => {}
                 None => {
-                    let mut res = Response::new(Body::from("Bad Request"));
-                    *res.status_mut() = StatusCode::BAD_REQUEST;
-                    return Ok(res);
+                    return Ok(bad_request(None));
                 }
             }
+        }
+        (&Method::POST, "/login") => {
+            let body = hyper::Body::to_bytes(req.into_body()).await?;
+            let args: LoginArgs = match serde_json::from_slice(body.as_ref()) {
+                Ok(args) => args,
+                Err(e) => return Ok(bad_request(Some(e.into()))),
+            };
+
+            tokio::task::block_in_place(move || match db.get(args.email.as_bytes()) {
+                Ok(Some(record_bytes)) => {
+                    let mut verifier = Verifier::default();
+                    let record: Web2AuthRecord = match serde_json::from_slice(record_bytes) {
+                        Ok(record) => record,
+                        Err(e) => return Ok(internal_server_error(Some(e.into()))),
+                    };
+                    match verifier
+                        .with_hash(record.password_hash)
+                        .with_password(args.password)
+                        .with_secret_key(hasher_secret.reveal_secret())
+                        .verify()
+                    {
+                        Ok(true) => {
+                            // TODO: create a JWT containing record.id, a nonce (will need to import an RNG for this) and probably some other stuff
+                            // TODO: return a response containing the JWT
+                        }
+                        Ok(false) => {
+                            // TODO: return response with 404 not found
+                        }
+                        Err(e) => {
+                            // TODO: print the error and return response with 505 internal server error
+                        }
+                    }
+                }
+                Ok(None) => {
+                    // TODO return response with 404 not found
+                }
+                Err(e) => {
+                    // TODO: print the error and return response with 505 internal server error
+                }
+            })
+        }
+        (&Method::POST, "/signup") => {
+            let body = hyper::body::to_bytes(req.into_body()).await?;
+            let args: SignupArgs = match serde_json::from_slice(body.as_ref()) {
+                Ok(args) => args,
+                Err(e) => return Ok(bad_request(Some(e.into()))),
+            };
+
+            tokio::task::block_in_place(move || {
+                let mut hasher = Hasher::default();
+                let password_hash = match hasher
+                    .with_password(args.password)
+                    .with_secret_key(hasher_secret.reveal_secret())
+                    .hash()
+                {
+                    Ok(hashed) => hashed,
+                    Err(e) => return Ok(internal_server_error(Some(e.into()))),
+                };
+                let record = Web2AuthRecord {
+                    password_hash: password_hash,
+                    id: Uuid::new_v4().as_u128(),
+                };
+                match db.put(args.email.as_bytes(), serde_json::to_vec(record).as_ref()) {
+                    Ok(res) => {
+                        let mut res = Response::new(Body::from("Created"));
+                        *res.status_mut() = StatusCode::CREATED;
+                        Ok(res)
+                    }
+                    Err(e) => return Ok(internal_server_error(Some(e.into()))),
+                }
+            })
         }
         _ => Ok(Response::new(Body::from(
             "Try POSTing data to /rpc such as: `curl localhost:3000/rpc -XPOST -d 'hello world'`",
@@ -72,7 +190,7 @@ struct JsonRPC {
     #[serde(default)]
     #[serde(skip_serializing_if = "Option::is_none")]
     params: Option<Value>,
-    id: i64 // for now, enforce ID to be a number
+    id: i64, // for now, enforce ID to be a number
 }
 
 fn main() {
@@ -100,10 +218,11 @@ fn main() {
                     let db = Arc::clone(&db);
                     Ok::<_, hyper::Error>(service_fn(move |req: Request<Body>| {
                         let db = Arc::clone(&db);
-                        handler(req, db)
+                        let jwt_secret = get_jwt_secret();
+                        let hasher_secret = get_hasher_secret();
+                        handler(req, db, jwt_secret, hasher_secret)
                     }))
                 }
-                
             });
 
             let server = Server::bind(&addr).serve(service_maker);
@@ -114,6 +233,6 @@ fn main() {
             }
         });
     }
-    
+
     // DB will automatically be closed when it gets dropped
 }
